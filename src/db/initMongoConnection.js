bf2254f6e9867c2a383f7063039c7d055ba4d2da
Exxin <exxinblood@gmail.com>
2024-06-17 16:02:47 +0300

    Update initMongoConnection.js

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -9,7 +9,7 @@ const MONGODB_DB = process.env.MONGODB_DB;
 export const initMongoConnection = async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`,
+      `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority&appName=Cluster0`,
     );
     console.log('Mongo connection successfully established!');
   } catch (error) {
