467683aac558684f1d6b7bf051ee3a9c20d14a35
Exxin <exxinblood@gmail.com>
2024-06-26 19:13:30 +0300

    fix_2

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,4 +1,4 @@
-import { Contact } from '../db/models/сontact.js';
+import { Contact } from '../db/models/Contact.js';
 
 import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
