8f0cfcec767db0feb28cc2017ca660ee1f1e7fb1
Exxin <exxinblood@gmail.com>
2024-07-11 22:23:19 +0300

    fix

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import router from './routers/index.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import cookieParser from 'cookie-parser';
+import { UPLOAD_DIR } from './constants/index.js';
 
 dotenv.config();
 
@@ -27,8 +28,9 @@ const setupServer = () => {
   app.use('*', notFoundHandler);
   app.use(errorHandler);
 
-  
-  app.listen(PORT, () => {
+  app.use('/uploads', express.static(UPLOAD_DIR));
+
+   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
 };
