8e40ed0d6e5b6bb1ce941826de462f54ab6efba5
Exxin <exxinblood@gmail.com>
2024-07-11 23:24:37 +0300

    Update initMongoConnection.js

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,15 +1,20 @@
 import mongoose from 'mongoose';
 
-const MONGODB_USER = process.env.MONGODB_USER;
-const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
-const MONGODB_URL = process.env.MONGODB_URL;
-const MONGODB_DB = process.env.MONGODB_DB;
+const MONGODB_USER = 'exxinblood';
+const MONGODB_PASSWORD = 'o4BsI9P9AjbFMDt9';
+const MONGODB_URL = 'cluster0.8cyzrat.mongodb.net';
+const MONGODB_DB = 'contacts';
+
+// const MONGODB_USER = process.env.MONGODB_USER;
+// const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
+// const MONGODB_URL = process.env.MONGODB_URL;
+// const MONGODB_DB = process.env.MONGODB_DB;
 
 
 export const initMongoConnection = async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`,
+      `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority&appName=Cluster0`,
     );
     console.log('Mongo connection successfully established!');
   } catch (error) {
@@ -17,4 +22,3 @@ export const initMongoConnection = async () => {
   }
 };
 
-//&appName=Cluster0
