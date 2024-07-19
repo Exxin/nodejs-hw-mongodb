1f7091bbb320a814801eb198ad5edd88fed85b51
Exxin <exxinblood@gmail.com>
2024-07-19 17:35:57 +0300

    upd

--- src/constants/index.js
+++ src/constants/index.js
@@ -18,12 +18,11 @@ export const CLOUDINARY = {
   CLOUD_NAME: process.env.CLOUD_NAME,
   API_KEY: process.env.API_KEY,
   API_SECRET: process.env.API_SECRET,
-  // CLOUD_NAME: 'dg7oyfk1d',
-  // API_KEY: '521692969348882',
-  // API_SECRET: 'nbIIMsLpfYflgNjkyq20ziTKVVs',
 };
 
 export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
 export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
 
 export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
+
+export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
--- src/routers/auth.js
+++ src/routers/auth.js
@@ -11,6 +11,9 @@ import { requestResetEmailSchema } from '../validation/auth.js';
 import { requestResetEmailController } from '../controllers/auth.js';
 import { resetPasswordSchema } from '../validation/auth.js';
 import { resetPasswordController } from '../controllers/auth.js';
+import { getGoogleOAuthUrlController } from '../controllers/auth.js';
+import {loginWithGoogleOAuthSchema} from '../validation/auth.js';
+import {loginWithGoogleController} from '../controllers/auth.js';
 
 const authRouter = Router();
 
@@ -46,5 +49,16 @@ authRouter.post(
   validateBody(resetPasswordSchema),
   ctrlWrapper(resetPasswordController),
 );
+
+
+authRouter.get('/get-oauth-url', ctrlWrapper(getGoogleOAuthUrlController));
+
+
+authRouter.post(
+  '/confirm-oauth',
+  validateBody(loginWithGoogleOAuthSchema),
+  ctrlWrapper(loginWithGoogleController),
+);
+
 
 export default authRouter;
