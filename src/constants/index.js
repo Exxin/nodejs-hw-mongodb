472a1c82a1e7d8d50d64e819fe2c95115bb43932
Exxin <exxinblood@gmail.com>
2024-08-01 14:16:51 +0300

    s

--- src/constants/index.js
+++ src/constants/index.js
@@ -15,9 +15,9 @@ export const SMTP = {
 };
 
 export const CLOUDINARY = {
-  CLOUD_NAME: process.env.CLOUD_NAME,
-  API_KEY: process.env.API_KEY,
-  API_SECRET: process.env.API_SECRET,
+  CLOUD_NAME: 'dg7oyfk1d',
+  API_KEY: '521692969348882',
+  API_SECRET: 'nbIIMsLpfYflgNjkyq20ziTKVVs',
   // CLOUD_NAME: 'dg7oyfk1d',
   // API_KEY: '521692969348882',
   // API_SECRET: 'nbIIMsLpfYflgNjkyq20ziTKVVs',
--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -1,6 +1,7 @@
-import cloudinary from 'cloudinary';
+// import cloudinary from 'cloudinary';
 import { CLOUDINARY } from '../constants/index.js';
 import 'dotenv/config';
+import { v2 as cloudinary } from 'cloudinary';
 
 const CLOUD_NAME = 'dg7oyfk1d';
 const API_KEY = '521692969348882';
