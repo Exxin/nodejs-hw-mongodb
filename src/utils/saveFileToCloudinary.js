57669987a7c6a074b2703b862b032c56c693e53c
Exxin <exxinblood@gmail.com>
2024-08-04 22:28:37 +0300

    x

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -1,20 +1,11 @@
-// import cloudinary from 'cloudinary';
+import cloudinary from 'cloudinary';
 import { CLOUDINARY } from '../constants/index.js';
-import 'dotenv/config';
-import { v2 as cloudinary } from 'cloudinary';
-
-// const CLOUDINARY.CLOUD_NAME = 'dg7oyfk1d';
-// const CLOUDINARY.API_KEY = '521692969348882';
-// const CLOUDINARY.API_SECRET = 'nbIIMsLpfYflgNjkyq20ziTKVVs';
 
 cloudinary.config({
   secure: true,
-  // cloud_name: CLOUDINARY.CLOUD_NAME,
-  // api_key: CLOUDINARY.API_KEY,
-  // api_secret: CLOUDINARY.API_SECRET,
-  cloud_name: 'dg7oyfk1d',
-  api_key: '521692969348882',
-  api_secret: 'nbIIMsLpfYflgNjkyq20ziTKVVs',
+  cloud_name: CLOUDINARY.CLOUD_NAME,
+  api_key: CLOUDINARY.API_KEY,
+  api_secret: CLOUDINARY.API_SECRET,
 });
 
 export const saveFileToCloudinary = async (file) => {
@@ -28,14 +19,3 @@ export const saveFileToCloudinary = async (file) => {
     throw new Error('Failed to upload file to Cloudinary');
   }
 };
-
-    //  const uploadResult = await cloudinary.uploader
-    //    .upload(
-    //        'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
-    //            public_id: 'shoes',
-    //        }
-    //    )
-    //    .catch((error) => {
-    //        console.log(error);
-    //    });
-    //    console.log(uploadResult);
