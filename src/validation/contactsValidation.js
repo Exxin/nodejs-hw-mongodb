cfe7b90ab2ea2086efc480e2f05905d19ed07fc0
Exxin <exxinblood@gmail.com>
2024-08-08 18:35:55 +0300

    f

--- src/validation/contactsValidation.js
+++ src/validation/contactsValidation.js
@@ -2,22 +2,32 @@ import Joi from 'joi';
 
 
 export const createContactsSchema = Joi.object({
-  name: Joi.string().min(3).max(20).required(),
+  name: Joi.string().min(3).max(30),
   phoneNumber: Joi.string().pattern(/^\+\d{12}$/).required().messages({
     'string.pattern.base': 'Phone number must be in the format +380... (12 digits after +)'
   }),
   email: Joi.string().email(),
   isFavourite: Joi.boolean().default(false),
   contactType: Joi.string().valid('work', 'home', 'personal').default('personal'),
+  age: Joi.number().integer().min(6).max(16),
+  gender: Joi.string().valid('male', 'female', 'other'),
+  avgMark: Joi.number().min(2).max(12),
+  onDuty: Joi.boolean(),
 });
 
 
-export const updateContactsSchema = Joi.object({
-  name: Joi.string().min(3).max(20),
-  phoneNumber: Joi.string().pattern(/^\+\d{10,16}$/),
+export const updateStudentSchema = Joi.object({
+  name: Joi.string().min(3).max(30),
+  phoneNumber: Joi.string().pattern(/^\+\d{12}$/).required().messages({
+    'string.pattern.base': 'Phone number must be in the format +380... (12 digits after +)'
+  }),
   email: Joi.string().email(),
-  isFavourite: Joi.boolean(),
-  contactType: Joi.string().valid('work', 'home', 'personal'),
+  isFavourite: Joi.boolean().default(false),
+  contactType: Joi.string().valid('work', 'home', 'personal').default('personal'),
+  age: Joi.number().integer().min(6).max(16),
+  gender: Joi.string().valid('male', 'female', 'other'),
+  avgMark: Joi.number().min(2).max(12),
+  onDuty: Joi.boolean(),
 });
 
 
