ec141ddf75d7656c5f2a938dde8c093a1735d39e
Exxin <exxinblood@gmail.com>
2024-08-08 19:05:00 +0300

    ппп

--- src/validation/contactsValidation.js
+++ src/validation/contactsValidation.js
@@ -1,22 +1,20 @@
 import Joi from 'joi';
 
 export const createContactsSchema = Joi.object({
-  name: Joi.string().min(3).max(30),
+  name: Joi.string().min(3).max(30).required(),
   phoneNumber: Joi.string().pattern(/^\+\d{12}$/).required().messages({
     'string.pattern.base': 'Phone number must be in the format +380... (12 digits after +)'
   }),
-  email: Joi.string().email(),
-  isFavourite: Joi.boolean().default(false),
-  contactType: Joi.string().valid('work', 'home', 'personal').default('personal'),
-  age: Joi.number().integer().min(6).max(16),
-  gender: Joi.string().valid('male', 'female', 'other'),
-  avgMark: Joi.number().min(2).max(12),
-  onDuty: Joi.boolean(),
+  email: Joi.string().email().required(),
+  isFavourite: Joi.boolean().default(false).required(),
+  contactType: Joi.string().valid('work', 'home', 'personal').default('personal').required(),
+  userId: Joi.string().required(),
+  photo: Joi.binary().required()
 });
 
 export const updateContactsSchema = Joi.object({
   name: Joi.string().min(3).max(30),
-  phoneNumber: Joi.string().pattern(/^\+\d{12}$/).required().messages({
+  phoneNumber: Joi.string().pattern(/^\+\d{12}$/).messages({
     'string.pattern.base': 'Phone number must be in the format +380... (12 digits after +)'
   }),
   email: Joi.string().email(),
@@ -26,6 +24,7 @@ export const updateContactsSchema = Joi.object({
   gender: Joi.string().valid('male', 'female', 'other'),
   avgMark: Joi.number().min(2).max(12),
   onDuty: Joi.boolean(),
+  photo: Joi.binary()
 });
 
 const dataToValidate = {
