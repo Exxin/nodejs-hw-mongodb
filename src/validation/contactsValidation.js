a2bc9bb1a4390f786fb2a9dfa6918c7882ebeec1
Exxin <exxinblood@gmail.com>
2024-08-13 16:28:15 +0300

    Update contactsValidation.js

--- src/validation/contactsValidation.js
+++ src/validation/contactsValidation.js
@@ -1,20 +1,8 @@
 import Joi from 'joi';
 
 export const createContactsSchema = Joi.object({
-  name: Joi.string().min(3).max(30).required(),
-  phoneNumber: Joi.string().pattern(/^\+\d{12}$/).required().messages({
-    'string.pattern.base': 'Phone number must be in the format +380... (12 digits after +)'
-  }),
-  email: Joi.string().email().required(),
-  isFavourite: Joi.boolean().default(false).required(),
-  contactType: Joi.string().valid('work', 'home', 'personal').default('personal').required(),
-  userId: Joi.string().required(),
-  photo: Joi.binary().required()
-});
-
-export const updateContactsSchema = Joi.object({
   name: Joi.string().min(3).max(30),
-  phoneNumber: Joi.string().pattern(/^\+\d{12}$/).messages({
+  phoneNumber: Joi.string().pattern(/^\+\d{12}$/).required().messages({
     'string.pattern.base': 'Phone number must be in the format +380... (12 digits after +)'
   }),
   email: Joi.string().email(),
@@ -24,7 +12,21 @@ export const updateContactsSchema = Joi.object({
   gender: Joi.string().valid('male', 'female', 'other'),
   avgMark: Joi.number().min(2).max(12),
   onDuty: Joi.boolean(),
-  photo: Joi.binary()
+});
+
+export const updateContactsSchema = Joi.object({
+  name: Joi.string().min(3).max(30).required(),
+  phoneNumber: Joi.string().required().pattern(/^\+\d{12}$/).messages({
+    'string.pattern.base': 'Phone number must be in the format +380... (12 digits after +)'
+  }),
+  email: Joi.string().email().required(),
+  isFavourite: Joi.boolean().default(false).required(),
+  contactType: Joi.string().valid('work', 'home', 'personal').default('personal').required(),
+  age: Joi.number().integer().min(6).max(16).required(),
+  gender: Joi.string().valid('male', 'female', 'other').required(),
+  avgMark: Joi.number().min(2).max(12).required(),
+  onDuty: Joi.boolean().required(),
+  photo: Joi.binary().required()
 });
 
 const dataToValidate = {
